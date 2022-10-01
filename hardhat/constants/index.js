3955a8d599c5f626b069234075424a48291d1512
Tushar Sabhani <tsabhani08@gmail.com>
2022-10-01 23:01:18 +0530

    initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x91ec25604a80603fE86B279DC90e589105eF278B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
